84316b2252188635c482463fba827f53da3ddf49
Jack Aitken <jaitk11@gmail.com>
2021-03-21 17:04:20 -0400

    working on exercises

---
+++ preparations/problem_5/answers.js
@@ -0,0 +1,11 @@
+/* 1. substring: Is a prototype method of the String constructor. 
+For example: */
+
+'hello'.substring(1, 3); // 'ell'
+
+/* 2. create: Is a static method of the Object constructor */
+
+/* 3. fromCharCode: Is a static method of the String constructor. It can be
+used without a string literal or string instance. For example: */
+
+String.fromCharCode(65); // 'A'
